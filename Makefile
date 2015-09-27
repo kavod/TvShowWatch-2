c5a7fef0c290b1e851abdf31e1a710d851628cbf
kavod <brice.grichy@gmail.com>
2015-09-27 16:28:44 +0200

    Improve install script

--- Makefile
+++ Makefile
@@ -1,12 +1,21 @@
+leftparen:=(
+rightparen:=)
+
 all: 
 	@echo "Usage:"
 	@echo "'sudo make install' for installation"
 	@echo "'sudo make uninstall' for remove installation"
 	
-install:JSAG
+install: JSAG
+install: tvdb-api
 
 JSAG:
 	@python utils/execute_color.py "Installing JsonSchemaAppGenerator" python utils/pkg_manager.py "install" "JSAG"
+	
+tvdb-api:
+	@if [ $(shell pip list|sed -n '/tvdb-api \${leftparen}.*\${rightparen}$$/p' |wc -l) -eq 0 ]; \
+	then python utils/execute_color.py "Installing tvdb_api" python utils/pkg_manager.py "install" "tvdb-api"; \
+	fi
 	
 uninstall:
 	@python utils/execute_color.py "Removing JsonSchemaAppGenerator" python utils/pkg_manager.py "uninstall" "JSAG"
--- utils/pkg_manager.py
+++ utils/pkg_manager.py
@@ -5,16 +5,47 @@ from __future__ import unicode_literals
 import sys
 import os
 import subprocess
+import getpass
 
 PACKAGES = {
-	'JSAG':{
-		'install':[
-			['wget',"https://github.com/kavod/jsonSchemaAppGenerator/releases/download/v1.5.10/JSAG.tgz",'-q'],
-			['tar','zxf','JSAG.tgz'],
-			['rm','JSAG.tgz']
+		'JSAG':{
+			'install':
+				[
+					['wget',"https://github.com/kavod/jsonSchemaAppGenerator/releases/download/v1.5.10/JSAG.tgz",'-q'],
+					['tar','zxf','JSAG.tgz'],
+					['rm','JSAG.tgz']
 				],
-		'uninstall':[
-			['rm','-rf','JSAG']
+			'uninstall':
+				[
+					['rm','-rf','JSAG']
+				],
+			'root_install':
+				[
+					['wget',"https://github.com/kavod/jsonSchemaAppGenerator/releases/download/v1.5.10/JSAG.tgz",'-q'],
+					['tar','zxf','JSAG.tgz'],
+					['rm','JSAG.tgz']
+				],
+			'root_uninstall':
+				[
+					['rm','-rf','JSAG']
+				]
+			},
+		'tvdb-api':{
+			'install':
+				[
+					['pip',"install",'--user','tvdb-api']
+				],
+			'uninstall':
+				[
+					['pip',"uninstall",'tvdb-api']
+				],
+			'root_install':
+				[
+					['pip',"install",'tvdb-api']
+				],
+			'root_uninstall':
+				[
+					['pip',"uninstall",'tvdb-api']
 				]
 			}
 		}
@@ -23,6 +54,8 @@ if len(sys.argv) < 3:
 	sys.exit(1)
 
 action = unicode(sys.argv[1])
+if getpass.getuser() == 'root':
+	action = 'root_' + action
 pkg = unicode(sys.argv[2])
 
 if pkg not in PACKAGES.keys():
