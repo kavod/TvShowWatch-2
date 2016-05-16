a39eca38eda65d7c9a08b2f08e54df051380ff6e
kavod <brice.grichy@gmail.com>
2016-05-16 11:02:24 +0200

    Fix sys importation issue

--- torrentSearch/torrentSearch.py
+++ torrentSearch/torrentSearch.py
@@ -10,8 +10,8 @@ import logging
 torrentProvider.loadProviders()
 
 class torrentSearch(JSAG3.JSAG3):
-	def __init__(self,id="torrentSearch",dataFile=None,verbosity=False):	
-		curPath = os.path.dirname(os.path.realpath(__file__))	
+	def __init__(self,id="torrentSearch",dataFile=None,verbosity=False):
+		curPath = os.path.dirname(os.path.realpath(__file__))
 		self.providers = dict()
 		JSAG3.JSAG3.__init__(self,
 			id=id,
@@ -20,11 +20,11 @@ class torrentSearch(JSAG3.JSAG3):
 			dataFile=dataFile,
 			verbosity=verbosity
 		)
-		
+
 	def loadConfig(self,confFile,path=[]):
 		self.addData(confFile)
 		logging.debug("[torrentSearch] Data loaded: {0}.".format(unicode(self.data)))
-		
+
 	def search(self,pattern):
 		if pattern is None or unicode(pattern) == "":
 			raise Exception("Empty pattern")
@@ -34,7 +34,7 @@ class torrentSearch(JSAG3.JSAG3):
 			self.connect_provider(provID)
 			for keyword in self.data['keywords']:
 				query = "{0} {1}".format(pattern,keyword)
-				
+
 				if 'keywords' in provider.keys() and len(provider['keywords']) > 0:
 					queries = ["{0} {1}".format(query,keyword) for keyword in provider['keywords']]
 				else:
@@ -49,7 +49,7 @@ class torrentSearch(JSAG3.JSAG3):
 						continue
 					return result
 		return None
-		
+
 	def search_query(self,provID,query):
 		torProv = self.providers[provID]
 		result = torProv.search(query)
@@ -65,14 +65,14 @@ class torrentSearch(JSAG3.JSAG3):
 		result = torProv.select_torrent(result)
 		result['provider'] = provID
 		return result
-		
-		
+
+
 	def connect_provider(self,provID):
 		provider = [provider for provider in self.data['providers'] if unicode(provider['provider_type']) == provID][0]
 		logging.info( "[torrentSearch] provider: ".format(unicode(self.data['providers'])))
 		if provID not in self.providers.keys():
 			self.providers[provID] = torrentProvider.torrentProvider(provID,provider['authentification'] if 'authentification' in provider.keys() else None)
-		
+
 	def download(self,tor):
 		availProviders = [prov['id'] for prov in torrentProvider.TRACKER_CONF]
 		if tor is None:
@@ -82,6 +82,3 @@ class torrentSearch(JSAG3.JSAG3):
 		if tor['provider'] not in availProviders:
 			raise Exception("Unknown torrent provider: {0} ({1})\nKnown providers: {2}".format(tor['provider'],type(tor['provider']),unicode(availProviders)))
 		return self.providers[tor['provider']].download(tor['id'])
-					
-					
-		
